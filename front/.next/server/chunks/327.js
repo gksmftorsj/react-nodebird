6b0896774e7e2eea62b1b8cb5049aa1542e41100
seunghyeon <ksh990913@gmail.com>
2023-04-14 00:37:27 +0900

    이미지 업로드 2개 이상 안되는 거 고침

--- front/.next/server/chunks/327.js
+++ front/.next/server/chunks/327.js
@@ -289,7 +289,7 @@ const reducer = (state = initialState, action) => {
 
       case UPLOAD_IMAGES_SUCCESS:
         {
-          draft.imagePaths = action.data;
+          draft.imagePaths = draft.imagePaths.concat(action.data);
           draft.uploadImagesLoading = false;
           draft.uploadImagesDone = true;
           break;
