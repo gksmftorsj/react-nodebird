42f156e46ebfbefbf558caf8778bb50c0f546169
seunghyeon <ksh990913@gmail.com>
2023-04-14 13:17:38 +0900

    "upload name 변경"

--- back/routes/post.js
+++ back/routes/post.js
@@ -35,7 +35,7 @@ const upload = multer({
     s3: new AWS.S3(),
     bucket: "react-nodebird-sss3",
     key(req, file, cb) {
-      cb(null, `original/${Date.now()}} ${path.basename(file.originalname)}`);
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
   }),
   // storage: multer.diskStorage({
